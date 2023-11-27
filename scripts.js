84565ac122c9074aad319c89cab51618d26df178
ibrahim <dicko.dev@gmail.com>
2023-11-27 11:32:07 +0000

    refonte porfolio +3

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 /* ======== animate multiple text ======= */
 var typed = new Typed(".multiple-text", {
-    strings: ["Frontend Developer", "Youtuber", "Blogger"],
+    strings: ["Mobile App Developer", "Youtuber", "Linux lover"],
     typeSpeed: 100, backSpeed: 100, backDelay: 1000, loop: true
 });
 
@@ -23,7 +23,7 @@ window.onscroll = () => {
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
-        
+
         if (top >= offset && offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
@@ -33,7 +33,7 @@ window.onscroll = () => {
     });
     /* ======== sticky navbar ======= */
     let header = document.querySelector('header');
-    
+
     header.classList.toggle('sticky', window.scrollY > 100);
 };
 
@@ -45,7 +45,101 @@ ScrollReveal({
     delay: 200,
 });
 
-ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', {origin: 'bottom'});
-ScrollReveal().reveal('.home-content h1, .about-img ', {origin: 'bottom'});
-ScrollReveal().reveal('.home-content p, .about-content ', {origin: 'right'});
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img ', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content p, .about-content ', { origin: 'right' });
+
+/* ======== Send Email ======= */
+
+const form = document.querySelector("form");
+
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const subject = document.getElementById("subject");
+const mess = document.getElementById("message");
+
+function sendEmail() {
+    const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.value} <br> Phone Number: ${phone.value} <br> Message: ${mess.value}`;
+    Email.send({
+        SecureToken: "b9252a2c-6ee4-4c10-b012-b9148f3b4f68",
+        To: 'dicko.dev@gmail.com',
+        From: "dicko.dev@gmail.com",
+        Subject: subject.value,
+        Body: bodyMessage,
+    }).then(
+        message => {
+            if (message == "OK") {
+                Swal.fire({
+                    title: "Success!",
+                    text: "Message sent successfully",
+                    icon: "success",
+                });
+            }
+        }
+    );
+};
+
+
+function checkInputs() {
+    const items = document.querySelectorAll(".item");
+    for (const item of items) {
+        if (item.value == "") {
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+
+        if (items[1].value != "") {
+            checkEmail();
+        }
+
+        items[1].addEventListener("keyup", () => {
+            checkEmail();
+        });
+
+        item.addEventListener("keyup", () => {
+            if (item.value != "") {
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            }
+            else {
+                item.classList.add("error");
+                item.parentElement.classList.add("error");
+            }
+        });
+    }
+};
+
+function checkEmail() {
+    const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+
+    const errorTxtEmail = document.querySelector(".error-txt.email");
+
+    if (!email.value.match(emailRegex)) {
+        email.classList.add("error");
+        email.parentElement.classList.add("error");
+
+        if (email.value != "") {
+            errorTxtEmail.innerHTML = "Enter a valid email address";
+        } else {
+            errorTxtEmail.innerHTML = "Email Address can't be blank";
+
+        }
+    } else {
+        email.classList.remove("error");
+        email.parentElement.classList.remove("error");
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkInputs();
+
+    if (!fullName.classList.contains("error") && !email.classList.contains("error") && !phone.classList.contains("error") && !subject.classList.contains("error") && !mess.classList.contains("error")) {
+        sendEmail();
+
+        form.reset();
+        return false;
+    }
+});
